27bb31f40ce7c449efa1a0711a913c6112c3c5ed
Vasylyna Andybur <vasylyna1206@yahoo.com>
2021-06-06 23:53:22 +0300

    task 03 done

---
+++ js/task-03.js
@@ -0,0 +1,46 @@
+//Напиши скрипт для создания галлереи изображений по массиву данных.
+// В HTML есть список ul#gallery.
+// <ul id="gallery"></ul>
+// Используй массив объектов images для создания тегов img вложенных в li.
+// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const gallery = document.querySelector("#gallery");
+
+images.forEach((image) => {
+  gallery.insertAdjacentHTML(
+    "afterbegin",
+    `<li><img src=${image.url}, alt='${image.alt}'></li>`
+  );
+  const itemList = document.querySelector("ul");
+  console.log(itemList);
+  itemList.classList.add("list");
+  const imageEl = document.querySelector("img");
+  imageEl.classList.add("img");
+});
+
+console.log(gallery);
+
+// const listItem = document.querySelector("li");
+// const image = document.querySelector("img");
+// image.setAttribute("url");
+// image.setAttribute("alt");
+
+// console.log(listItem);
+// console.log(image);
